47d29cef64b5f91fbfdb6ec9805cf5b3c4953a2c
Tony Ghita <tony@twitch.tv>
2021-02-17 16:56:38 -0800

    Simplify test function

--- repro_test.go
+++ repro_test.go
@@ -7,11 +7,9 @@ import (
 )
 
 func TestF(t *testing.T) {
-	t.Run("F", func(t *testing.T) {
-		var a *repro.A
+	var a *repro.A
 
-		if a.F() != nil {
-			t.Fail()
-		}
-	})
+	if a.F() != nil {
+		t.Fail()
+	}
 }
